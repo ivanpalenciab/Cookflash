3ba28bd4efdb79bdc49e56ef792526a77de9e2b1
Ivan Palencia <ivanpalenciab@gmail.com>
2022-11-09 19:04:17 -0500

    commit transicional

---
+++ proyecto-front/src/componentes/planes/planes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Planes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
