ef8b8b6db96a89d62f157351f62d991d7f7a9c0f
Ivan Palencia <ivanpalenciab@gmail.com>
2022-11-19 19:33:41 -0500

    Creamos un crud para recetas

--- proyecto_back/controllers/recetas.controller.js
+++ proyecto_back/controllers/recetas.controller.js
@@ -35,8 +35,62 @@ exports.create = function (req, res) {
   });
 };
 
+//Listar todos las recetas
 exports.find = function(req,res){
   Receta.find(function(err,receta){
     res.json(receta)
 })
+}
+//Obtener una receta
+exports.findOne = function(req,res){
+  Receta.findOne({_id:req.params.id}, function(err,receta){
+    res.json(receta)
+  })
+}
+
+//Actualizar una receta
+exports.update = function(req,res){
+  /*let receta = {
+    nombre_receta: req.body.nombre_receta,
+    tipo_receta: req.body.tipo_receta,
+    descripcion: req.body.descripcion,
+    clasificacion: req.body.clasificacion,
+    origen: req.body.origen,
+    tiempo_preparacion: req.body.tiempo_preparacion,
+    tiempo_coccion: req.body.tiempo_coccion,
+    tiempo_refrigeracion: req.body.tiempo_refrigeracion,
+    ingredientes:req.body.ingredientes,
+    preparacion: req.body.preparacion,
+    tips_adicionales: req.body.tips_adicionales,
+    calificacion: req.body.calificacion
+  }*/
+
+  Receta.findByIdAndUpdate(req.params.id,{$set:req.body}, function(err){
+    if (err){
+      console.error(err),
+      response.exito=false,
+      response.msg = "Error al modificar la receta"
+      res.json(response)
+      return;
+    }
+    response.exito = true,
+    response.msg = "receta modificada correctamente"
+    res.json(response)
+  })
+}
+
+/*Esta funcion es para borrar recetas */
+exports.remove = function(req,res){
+  Receta.findByIdAndRemove({_id:req.params.id}, function(err){
+    if(err){
+      console.error(err),
+      response.exito=false,
+      response.msg = err
+      res.json(response)
+      return;
+    }
+    response.exito=true
+    response.msg = "receta eliminada correctamente"
+    res.json(response)
+  })
 }
--- proyecto_back/routes/recetas.router.js
+++ proyecto_back/routes/recetas.router.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const recetasController = require("../controllers/recetas.controller");
 
-router.post("/create", recetasController.create)
-router.get("/", recetasController.find)
+router.post("/create", recetasController.create) //ok
+router.get("/", recetasController.find)//ok
+router.get("/:id",recetasController.findOne)//ok
+router.delete("/:id",recetasController.remove)//ok
+router.put("/:id",recetasController.update)
+
 
 module.exports = router
